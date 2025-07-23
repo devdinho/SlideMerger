0da1b80ddee42f1d8d84729162a650a240717d15
devdinho <freitas.dev@proton.me>
2025-07-23 06:23:58 -0300

    Add prod url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
     },
   },
   server: {
-    allowedHosts: ['localhost', '127.0.0.1', 'fine-monkeys-cross.loca.lt'],
+    allowedHosts: ['localhost', '127.0.0.1', 'mbx-standardizer.dinho.dev'],
   },
 })
